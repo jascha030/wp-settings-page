fa6a694b0157032b81b483bcaf272908bb25fb8f
Jascha van Aalst <mr.jascha@gmail.com>
2020-04-07 12:40:13 +0200

    Added file input type and MIME types

--- src/HtmlField.php
+++ src/HtmlField.php
@@ -9,13 +9,15 @@ class HtmlField
     const RADIO = 2;
     const CHECKBOX = 3;
     const SELECT = 4;
+    const FILE = 5;
 
     const HTML_INPUT_TYPES = [
         self::TEXT     => "text",
         self::TEXTAREA => "textarea",
         self::RADIO    => "radio",
         self::CHECKBOX => "checkbox",
-        self::SELECT   => "select"
+        self::SELECT   => "select",
+        self::FILE     => "file",
     ];
 
     const REGULAR_INPUT_TYPES = [
@@ -27,6 +29,49 @@ class HtmlField
         self::CHECKBOX
     ];
 
+    const FILE_TYPES = [
+        'image' => self::IMAGE_TYPES,
+        'application' => self::APPLICATION_FILE_TYPES,
+        'audio' => self::AUDIO_FILE_TYPES,
+        'video' => self::VIDEO_FILE_TYPES
+    ];
+
+
+    const IMAGE_TYPES = [
+        'png',
+        'jpeg',
+        'gif',
+        'x-icon',
+        'vnd.adobe.photoshop'
+    ];
+
+    const APPLICATION_FILE_TYPES = [
+        'pdf',
+        'msword',
+        'vnd.openxmlformats-officedocument.wordprocessingml.document',
+        'application/vnd.ms-powerpoint',
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        'application/vnd.oasis.opendocument.text',
+        'vnd.ms-excel',
+        'vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ];
+
+    const AUDIO_FILE_TYPES = [
+        'mpeg',
+        'm4a',
+        'ogg',
+        'wav'
+    ];
+
+    const VIDEO_FILE_TYPES = [
+        'mp4',
+        'quicktime',
+        'x-ms-wmv',
+        'avi',
+        'mpeg'
+    ];
+
+
     /**
      * @param int $fieldType
      *
@@ -35,6 +80,21 @@ class HtmlField
     public static function validateFieldType(int $fieldType): bool
     {
         return (array_key_exists($fieldType, self::HTML_INPUT_TYPES));
+    }
+
+    public static function validateMimeType(string $mime): bool
+    {
+        $sections = explode('/', $mime);
+
+        if (!array_key_exists($sections[0], self::FILE_TYPES)) {
+            return false;
+        }
+
+        if (!in_array(self::FILE_TYPES[$sections[0]], $sections[1])) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
